7dd284ac7b62e658857854d712adca5523ba013b
samuwils <samwilson3360@gmail.com>
2024-03-09 21:24:05 -0800

    first commit

---
+++ Debug/subdir_rules.mk
@@ -0,0 +1,43 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Each subdirectory must supply rules for building sources it contributes
+%.o: ../%.c $(GEN_OPTS) | $(GEN_FILES) $(GEN_MISC_FILES)
+	@echo 'Building file: "$<"'
+	@echo 'Invoking: GNU Compiler'
+	"C:/ti/gcc_arm_none_eabi_9_2_1/bin/arm-none-eabi-gcc-9.2.1.exe" -c @"device.opt"  -mcpu=cortex-m0plus -march=armv6-m -mthumb -mfloat-abi=soft -I"C:/Users/samwi/workspace_v12/epaper_1_54_testing" -I"C:/Users/samwi/workspace_v12/epaper_1_54_testing/Debug" -I"C:/ti/mspm0_sdk_1_30_00_03/source/third_party/CMSIS/Core/Include" -I"C:/ti/mspm0_sdk_1_30_00_03/source" -I"C:/ti/gcc_arm_none_eabi_9_2_1/arm-none-eabi/include/newlib-nano" -I"C:/ti/gcc_arm_none_eabi_9_2_1/arm-none-eabi/include" -O2 -ffunction-sections -fdata-sections -g -gdwarf-3 -gstrict-dwarf -Wall -MMD -MP -MF"$(basename $(<F)).d_raw" -MT"$(@)" -std=c99 $(GEN_OPTS__FLAG) -o"$@" "$<"
+	@echo 'Finished building: "$<"'
+	@echo ' '
+
+build-1938710943: ../epaper_1_54_testing.syscfg
+	@echo 'Building file: "$<"'
+	@echo 'Invoking: SysConfig'
+	"C:/ti/ccs1260/ccs/utils/sysconfig_1.19.0/sysconfig_cli.bat" --script "C:/Users/samwi/workspace_v12/epaper_1_54_testing/epaper_1_54_testing.syscfg" -o "." -s "C:/ti/mspm0_sdk_1_30_00_03/.metadata/product.json" --compiler gcc
+	@echo 'Finished building: "$<"'
+	@echo ' '
+
+device_linker.lds: build-1938710943 ../epaper_1_54_testing.syscfg
+device.opt: build-1938710943
+device.lds.genlibs: build-1938710943
+ti_msp_dl_config.c: build-1938710943
+ti_msp_dl_config.h: build-1938710943
+Event.dot: build-1938710943
+
+%.o: ./%.c $(GEN_OPTS) | $(GEN_FILES) $(GEN_MISC_FILES)
+	@echo 'Building file: "$<"'
+	@echo 'Invoking: GNU Compiler'
+	"C:/ti/gcc_arm_none_eabi_9_2_1/bin/arm-none-eabi-gcc-9.2.1.exe" -c @"device.opt"  -mcpu=cortex-m0plus -march=armv6-m -mthumb -mfloat-abi=soft -I"C:/Users/samwi/workspace_v12/epaper_1_54_testing" -I"C:/Users/samwi/workspace_v12/epaper_1_54_testing/Debug" -I"C:/ti/mspm0_sdk_1_30_00_03/source/third_party/CMSIS/Core/Include" -I"C:/ti/mspm0_sdk_1_30_00_03/source" -I"C:/ti/gcc_arm_none_eabi_9_2_1/arm-none-eabi/include/newlib-nano" -I"C:/ti/gcc_arm_none_eabi_9_2_1/arm-none-eabi/include" -O2 -ffunction-sections -fdata-sections -g -gdwarf-3 -gstrict-dwarf -Wall -MMD -MP -MF"$(basename $(<F)).d_raw" -MT"$(@)" -std=c99 $(GEN_OPTS__FLAG) -o"$@" "$<"
+	@echo 'Finished building: "$<"'
+	@echo ' '
+
+startup_mspm0g350x_gcc.o: C:/ti/mspm0_sdk_1_30_00_03/source/ti/devices/msp/m0p/startup_system_files/gcc/startup_mspm0g350x_gcc.c $(GEN_OPTS) | $(GEN_FILES) $(GEN_MISC_FILES)
+	@echo 'Building file: "$<"'
+	@echo 'Invoking: GNU Compiler'
+	"C:/ti/gcc_arm_none_eabi_9_2_1/bin/arm-none-eabi-gcc-9.2.1.exe" -c @"device.opt"  -mcpu=cortex-m0plus -march=armv6-m -mthumb -mfloat-abi=soft -I"C:/Users/samwi/workspace_v12/epaper_1_54_testing" -I"C:/Users/samwi/workspace_v12/epaper_1_54_testing/Debug" -I"C:/ti/mspm0_sdk_1_30_00_03/source/third_party/CMSIS/Core/Include" -I"C:/ti/mspm0_sdk_1_30_00_03/source" -I"C:/ti/gcc_arm_none_eabi_9_2_1/arm-none-eabi/include/newlib-nano" -I"C:/ti/gcc_arm_none_eabi_9_2_1/arm-none-eabi/include" -O2 -ffunction-sections -fdata-sections -g -gdwarf-3 -gstrict-dwarf -Wall -MMD -MP -MF"$(basename $(<F)).d_raw" -MT"$(@)" -std=c99 $(GEN_OPTS__FLAG) -o"$@" "$<"
+	@echo 'Finished building: "$<"'
+	@echo ' '
+
+
---
+++ Debug/subdir_vars.mk
@@ -0,0 +1,70 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+SYSCFG_SRCS += \
+../epaper_1_54_testing.syscfg 
+
+C_SRCS += \
+../epaper_1_54_testing.c \
+./ti_msp_dl_config.c \
+C:/ti/mspm0_sdk_1_30_00_03/source/ti/devices/msp/m0p/startup_system_files/gcc/startup_mspm0g350x_gcc.c 
+
+GEN_CMDS += \
+./device_linker.lds 
+
+GEN_FILES += \
+./device_linker.lds \
+./device.opt \
+./ti_msp_dl_config.c 
+
+C_DEPS += \
+./epaper_1_54_testing.d \
+./ti_msp_dl_config.d \
+./startup_mspm0g350x_gcc.d 
+
+GEN_OPTS += \
+./device.opt 
+
+OBJS += \
+./epaper_1_54_testing.o \
+./ti_msp_dl_config.o \
+./startup_mspm0g350x_gcc.o 
+
+GEN_MISC_FILES += \
+./device.lds.genlibs \
+./ti_msp_dl_config.h \
+./Event.dot 
+
+OBJS__QUOTED += \
+"epaper_1_54_testing.o" \
+"ti_msp_dl_config.o" \
+"startup_mspm0g350x_gcc.o" 
+
+GEN_MISC_FILES__QUOTED += \
+"device.lds.genlibs" \
+"ti_msp_dl_config.h" \
+"Event.dot" 
+
+C_DEPS__QUOTED += \
+"epaper_1_54_testing.d" \
+"ti_msp_dl_config.d" \
+"startup_mspm0g350x_gcc.d" 
+
+GEN_FILES__QUOTED += \
+"device_linker.lds" \
+"device.opt" \
+"ti_msp_dl_config.c" 
+
+C_SRCS__QUOTED += \
+"../epaper_1_54_testing.c" \
+"./ti_msp_dl_config.c" \
+"C:/ti/mspm0_sdk_1_30_00_03/source/ti/devices/msp/m0p/startup_system_files/gcc/startup_mspm0g350x_gcc.c" 
+
+SYSCFG_SRCS__QUOTED += \
+"../epaper_1_54_testing.syscfg" 
+
+
